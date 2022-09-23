d325d77b0aeef8c2860997c9f0d16358eaea9f96
ookuki <77297879+ookuki@users.noreply.github.com>
2022-09-24 04:34:01 +0900

    Epic/version3 (#6)

--- build.js
+++ build.js
@@ -1,18 +1,17 @@
 const builder = require('electron-builder')
 const Platform = builder.Platform
+const fs = require('fs-extra')
+const path = require('path')
+const root = require('app-root-path')
+const Util = require('./app/assets/js/util')
 
-function getCurrentPlatform(){
-    switch(process.platform){
-        case 'win32':
-            return Platform.WINDOWS
-        case 'darwin':
-            return Platform.MAC
-        case 'linux':
-            return Platform.linux
-        default:
-            console.error('Cannot resolve current platform!')
-            return undefined
-    }
+const dirName = 'jdk'
+const basePath = path.join(root.path, dirName)
+
+if (!fs.existsSync(basePath)) {
+    console.log(`\x1b[31mERROR!!: ${basePath} が見つかりません`)
+    console.log("\x1b[31mERROR!!: https://github.com/TeamKun/config-files/releases/download/jdk/jdk.zip をダウンロードして展開してください")
+    return
 }
 
 builder.build({
@@ -21,18 +20,16 @@ builder.build({
         appId: 'numalauncher',
         productName: 'NumaLauncher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi, 2020 Kamesuta',
+        copyright: 'Copyright © 2018-2020 Daniel Scalzi, 2020 TeamKUN',
         directories: {
             buildResources: 'build',
             output: 'dist'
         },
         win: {
-            target: [
-                {
-                    target: 'nsis',
-                    arch: 'x64'
-                }
-            ]
+            target: [{
+                target: 'nsis',
+                arch: 'x64'
+            }]
         },
         nsis: {
             oneClick: false,
@@ -46,8 +43,8 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi, Kamesuta',
-            vendor: 'Daniel Scalzi, Kamesuta',
+            maintainer: 'Daniel Scalzi, TeamKUN',
+            vendor: 'Daniel Scalzi, TeamKUN',
             synopsis: '沼でも使えるMinecraftランチャー',
             description: '参加型に参加するためのすべてがここに。Mod、コンフィグ、アップデートが全自動で揃います。',
             category: 'Game'
@@ -57,7 +54,8 @@ builder.build({
             '!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js,numa_skins.json,skinSetting.json}'
         ],
         extraResources: [
-            'libraries'
+            'libraries',
+            `jdk/${getJDKDirectory()}`
         ],
         asar: true
     }
@@ -66,3 +64,31 @@ builder.build({
 }).catch(err => {
     console.error('Error during build!', err)
 })
+
+function getCurrentPlatform() {
+    switch (process.platform) {
+        case 'win32':
+            return Platform.WINDOWS
+        case 'darwin':
+            return Platform.MAC
+        case 'linux':
+            return Platform.linux
+        default:
+            console.error('Cannot resolve current platform!')
+            return undefined
+    }
+}
+
+function getJDKDirectory() {
+    switch (process.argv[2]) {
+        case 'WINDOWS':
+            return 'windows'
+        case 'mac':
+            if (isAappleSilicon()) {
+                return 'mac-apple'
+            }
+            return 'mac-intel'
+        case 'LINUX':
+            return 'linux'
+    }
+}
